fbf65e2a18c87a6037038fdb484971df665c443d
Georg <48410861+weltbester@users.noreply.github.com>
2019-10-07 15:15:39 +0200

    created C++ template

---
+++ test.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright: Georg Pohl, 70174 Stuttgart, et al
+ * Filename: test.cpp
+ */
+ #include <iostream>
+ using namespace std;
+ 
+ /* CONSTANTS */
+ 
+ 
+/* Instance Variables */
+
+/* Prototypes */
+
+int main() {
+  
+  cout << endl;
+  return 0;
+} // main
+
+/* Functions */
