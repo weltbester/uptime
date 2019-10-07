a501520fbd54d6aae8db7517953cfd7ea49e6dc3
Georg <georgpohl@gmx.de>
2019-10-07 13:21:55 +0000

    nix

--- test.cpp
+++ test.cpp
@@ -1,19 +1,19 @@
 /*
- * Copyright: Georg Pohl, 70174 Stuttgart, et al
+ * Copyright: Georg Pohl, 70174 Stuttgart, et al.
  * Filename: test.cpp
  */
  #include <iostream>
  using namespace std;
- 
+
  /* CONSTANTS */
- 
- 
+
+
 /* Instance Variables */
 
 /* Prototypes */
 
 int main() {
-  
+
   cout << endl;
   return 0;
 } // main
